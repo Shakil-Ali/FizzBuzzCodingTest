846722540670a201873963fb557ee8e576025d2c
Shakil Ali <shakil_419@hotmail.co.uk>
2024-03-11 16:09:01 +0000

    Added logic to output correct word depending on integer

--- FizzBuzzCodingTest/FizzBuzzMain.cs
+++ FizzBuzzCodingTest/FizzBuzzMain.cs
@@ -5,7 +5,21 @@ namespace FizzBuzzCodingTest
     {
         public string GetFizzBuzzOutput(int number)
         {
-            return number.ToString();
+
+            // Logic to workout which string to return
+
+            if (number % 3 == 0)
+                return "Fizz";
+            else if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else if (number % 5 == 0 && number % 3 == 0) 
+            {
+                return "FizzBuzz";
+            }
+            else
+                return number.ToString();
         }
     }
 }
