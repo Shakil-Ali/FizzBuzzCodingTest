5c5e0cb418070b100a8b804cfd47e5e140c74243
Shakil Ali <shakil_419@hotmail.co.uk>
2024-03-11 17:28:05 +0000

    New main file to run the code

---
+++ FizzBuzzCodingTest/Main.cs
@@ -0,0 +1,15 @@
+﻿using FizzBuzzChallenge;
+using System;
+
+namespace FizzBuzzCodingTest
+{
+    public class MainProgram
+    {
+        public static void Main(string[] args)
+        {
+            var fizzBuzz = new FizzBuzz();
+            fizzBuzz.PrintFizzBuzzRange(1, 100);
+
+        }
+    }
+}
