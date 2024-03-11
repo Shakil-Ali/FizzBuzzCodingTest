562c7f1e769f5980c045d748fab5120e1b916ae0
Shakil Ali <shakil_419@hotmail.co.uk>
2024-03-11 17:14:22 +0000

    Altered the code so that the check for 3 and 5 comes first, then the rest follow.

--- FizzBuzzCodingTest/FizzBuzzMain.cs
+++ FizzBuzzCodingTest/FizzBuzzMain.cs
@@ -1,23 +1,19 @@
-﻿
-namespace FizzBuzzCodingTest
+﻿using System;
+
+namespace FizzBuzzChallenge
 {
-    public class FizzBuzzMain
+    public class FizzBuzz
     {
-        public string GetFizzBuzzOutput(int number)
+        public string GetFizzBuzz(int number)
         {
+            // Logic for finding which term to output
 
-            // Logic to workout which string to return
-
-            if (number % 3 == 0)
+            if (number % 3 == 0 && number % 5 == 0)
+                return "FizzBuzz";
+            else if (number % 3 == 0)
                 return "Fizz";
             else if (number % 5 == 0)
-            {
                 return "Buzz";
-            }
-            else if (number % 5 == 0 && number % 3 == 0) 
-            {
-                return "FizzBuzz";
-            }
             else
                 return number.ToString();
         }
