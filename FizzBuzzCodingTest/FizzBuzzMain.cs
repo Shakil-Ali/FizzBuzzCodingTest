e81f186806fe2d9e3a354d5c36043a5ce35fbd2f
Shakil Ali <shakil_419@hotmail.co.uk>
2024-03-11 16:01:36 +0000

    FizzBuzzMain file created with the initial skeleton of the main file. Removed file which was not required.

---
+++ FizzBuzzCodingTest/FizzBuzzMain.cs
@@ -0,0 +1,11 @@
+﻿
+namespace FizzBuzzCodingTest
+{
+    public class FizzBuzzMain
+    {
+        public string GetFizzBuzzOutput(int number)
+        {
+            return number.ToString();
+        }
+    }
+}
